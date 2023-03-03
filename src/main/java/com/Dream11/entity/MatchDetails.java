894dce14b8b3db7d9d5684574977e084d2f8fc84
Shreya Maheshwari <shreyam803@gmail.com>
2023-03-03 12:04:18 +0530

    created entities

---
+++ src/main/java/com/Dream11/entity/MatchDetails.java
@@ -0,0 +1,18 @@
+package com.Dream11.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Document(collection = "MatchDetails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatchDetails {
+    @Id
+    private int matchId;
+    private int team1Id;
+    private int team2Id;
+    private int team1Score;
+    private int team2Score;
+}
---
+++ src/main/java/com/Dream11/entity/PlayerTitle.java
@@ -0,0 +1,8 @@
+package com.Dream11.entity;
+public enum PlayerTitle {
+    STRONG_BATSMAN,
+    STRONG_BOWLER,
+    ALL_ROUNDER,
+    AVERAGE_BATSMAN,
+    AVERAGE_BOWLER
+}
