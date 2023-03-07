65109e021e94336175a639c5aa085175720a8fbc
Akshat <akshatchauhaniit@gmail.com>
2023-03-07 14:31:39 +0530

    Unmerged with hritik

---
+++ src/main/java/com/Dream11/utility/ApplicationUtils.java
@@ -0,0 +1,12 @@
+package com.Dream11.utility;
+
+
+public class ApplicationUtils {
+    public static int _RUNS = 6;
+    public static int WICKET = 7;
+    public static int FOUR_RUNS = 4;
+    public static int ONE_RUN = 1;
+    public static int TWO_RUNS = 2;
+    public static int THREE_RUNS = 3;
+
+}
